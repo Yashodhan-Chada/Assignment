f7bac3e98d52a0981632c082ae9a04047519c1d6
Yashodhan-Chada <121384344+Yashodhan-Chada@users.noreply.github.com>
2022-12-25 20:09:33 +0530

    Lists, sets, tuples, dictionaries and applications of r+, w+, a+

---
+++ Assignment_Yashodhan.py
@@ -0,0 +1,7 @@
+r+
+Open for reading and writing. The stream is positioned at the beginning of the file.
+w+
+Opens a file in read and write mode. It creates a new file if it does not exist, if it exists, 
+it erases the contents of the file and the file pointer starts from the beginning.
+a+
+Open for reading and writing. The file is created if it does not exist. The stream is positioned at the end of the file.
