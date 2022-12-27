db91b4fa42551cf2a99d4f6a08e8c76c3ed32788
Yashodhan-Chada <121384344+Yashodhan-Chada@users.noreply.github.com>
2022-12-27 18:34:15 +0530

    Update Assignment_Yashodhan.py

--- Assignment_Yashodhan.py
+++ Assignment_Yashodhan.py
@@ -162,12 +162,104 @@ Dictionary with the use of Integer Keys:
 Dictionary with the use of Mixed Keys: 
 {'Name': 'Yash', 1: [1, 2, 3, 4]}
 
+Add Elements to a Dictionary
+capital_city = {"Nepal": "Kathmandu", "England": "London"}
+print("Initial Dictionary: ",capital_city)
+capital_city["Japan"] = "Tokyo"
+print("Updated Dictionary: ",capital_city)
+Output
+Initial Dictionary:  {'Nepal': 'Kathmandu', 'England': 'London'}
+Updated Dictionary:  {'Nepal': 'Kathmandu', 'England': 'London', 'Japan': 'Tokyo'}
+  
+Removing elements from Dictionary
 
+student_id = {111: "Eric", 112: "Kyle", 113: "Butters"}
+print("Initial Dictionary: ", student_id)
+del student_id[111]
+print("Updated Dictionary ", student_id)
+Output
+Initial Dictionary:  {111: 'Eric', 112: 'Kyle', 113: 'Butters'}
+Updated Dictionary  {112: 'Kyle', 113: 'Butters'}
 
+Iterating Through a Dictionary
+# Iterating through a Dictionary
+squares = {1: 1, 3: 9, 5: 25, 7: 49, 9: 81}
+for i in squares:
+    print(squares[i])
+Output
+1
+9
+25
+49
+81
 
+Sets
+Create a Set 
+# create a set of integer type
+student_id = {112, 114, 116, 118, 115}
+print('Student ID:', student_id)
+# create a set of string type
+vowel_letters = {'a', 'e', 'i', 'o', 'u'}
+print('Vowel Letters:', vowel_letters)
+# create a set of mixed data types
+mixed_set = {'Hello', 101, -2, 'Bye'}
+print('Set of mixed data types:', mixed_set)
+Output
+Student ID: {112, 114, 115, 116, 118}
+Vowel Letters: {'u', 'a', 'e', 'i', 'o'}
+Set of mixed data types: {'Hello', 'Bye', 101, -2}
 
+Add Items to a Set 
+numbers = {21, 34, 54, 12}
+print('Initial Set:',numbers)
+# using add() method
+numbers.add(32)
+print('Updated Set:', numbers) 
+Output
+Initial Set: {34, 12, 21, 54}
+Updated Set: {32, 34, 12, 21, 54}
 
+Remove an Element from a Set
+languages = {'Swift', 'Java', 'Python'}
+print('Initial Set:',languages)
+# remove 'Java' from a set
+removedValue = languages.discard('Java')
+print('Set after remove():', languages)
+Output
+Initial Set: {'Python', 'Swift', 'Java'}
+Set after remove(): {'Python', 'Swift'}
 
+Iterate Over a Set in Python
+fruits = {"Apple", "Peach", "Mango"}
+# for loop to access each fruits
+for fruit in fruits: 
+    print(fruit)
+Output
+Mango
+Peach
+Apple
 
+frozenset()
+Example 1: Working of frozenset()
+# tuple of vowels
+vowels = ('a', 'e', 'i', 'o', 'u')
+fSet = frozenset(vowels)
+print('The frozen set is:', fSet)
+print('The empty frozen set is:', frozenset())
+# frozensets are immutable
+fSet.add('v')
+Output
+The frozen set is: frozenset({'a', 'o', 'u', 'i', 'e'})
+The empty frozen set is: frozenset()
+Traceback (most recent call last):
+  File "<string>, line 8, in <module>
+    fSet.add('v')
+AttributeError: 'frozenset' object has no attribute 'add'
 
-
+Example 2: frozenset() for Dictionary
+# random dictionary
+person = {"name": "John", "age": 23, "sex": "male"}
+fSet = frozenset(person)
+print('The frozen set is:', fSet)
+Output
+The frozen set is: frozenset({'name', 'sex', 'age'})
